82f5e281ab025de4eaf10d944ab23bf05db647b8
Pete Carkeek <petecarkeek@gmail.com>
2019-05-20 18:45:22 -0700

    update twoDiff.py

--- src/twoDiff.py
+++ src/twoDiff.py
@@ -3,14 +3,14 @@
 pairs =set()
 
 target = 1
-list = [1,2,3,4,2]
+list = [1,2,3,2,5,6]
 
 for i in range(len(list)-1):
-    for j in range(1,len(list)):
+    for j in range(i+1,len(list)):
         tuple = (list[i],list[j])
         if list[j] - list[i] == target:
-            if not tuple in pairs:
+            if tuple not in pairs:
                 pairs.add(tuple)
 
-print(tuple)
+print(pairs)
         
