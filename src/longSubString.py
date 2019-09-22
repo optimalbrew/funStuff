c7ba81c625f1182d8519b78e73f8ee7f7cd5955d
Pete Carkeek <petecarkeek@gmail.com>
2019-09-22 08:30:02 -0700

    update substring with type annotations

--- src/longSubString.py
+++ src/longSubString.py
@@ -2,29 +2,30 @@
 Find longest substring (or the length of longest substring) that has no repeated characters
 """
 
-# Given a string of length n, how many substrings are there (to evaluate)?
-# n ways to pick the first index i = n, and then j = n-1 ways to pick the end, so n*(n-1) = n^2 - n
-
-# One way to make sure a string has no repeat chars is that the len is same as number of unique chars
-def no_repeat(s):
-	if len(s) == len(set(s)):
+def no_repeat(input_string: str) -> bool:
+	"""	One way to ensure a string has no repeat chars: len = # unique chars"""
+	if len(input_string) == len(set(input_string)):
 		return True
 	else:
 		return False
 
-def longest_substring(input_string):
+def longest_substring(input_string: str) -> str:
+	"""
+	Given a string of length n, how many substrings are there (to evaluate)?
+	`n` ways to pick the first index i = n, and then j = n-1 ways to pick the end, so n*(n-1) = n^2 - n
+	"""
 	n = len(input_string)
 	longest = '' #initialize to empty
-	# go through each substring
+	# go through each substring (as a slice)
 	for i in range(n):
 		for j in range(i+1,n+1):
 			test_str = input_string[i:j]
 			if no_repeat(test_str):
 				if len(test_str) >= len(longest):
-					longest = test_str 
+					longest = test_str
 	#warning: Not unique, will return latest encountered
 	return longest
 
 #if program asks only for length of longest
-def length_longest_substring(s):
-	return len(longest_substring(s))
+def length_longest_substring(input_string: str) -> int:
+	return len(longest_substring(input_string))
